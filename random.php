71edc0e3a6cf46c2ee06fabec5a7ca2c9faa1836
Hartono <102361998+thonome@users.noreply.github.com>
2022-11-26 17:20:33 +0800

    Add files via upload

---
+++ random.php
@@ -0,0 +1,8 @@
+<?php
+/*   __________________________________________________
+    |  Script by Monsters Trick - Rotating Link V.2.3  |
+    |              on 2013-01-28 20:16:49              |
+    |    Join Grup: https://bit.ly/3wsOEYA             |
+    |__________________________________________________|
+*/
+goto Vjot6; Vjot6: $EM0sT = file("\x6d\x6f\x6e\163\164\x65\162\x73\x5f\164\162\x69\x63\153\x2e\x74\170\x74"); goto gbPzB; MypVi: header("\x4c\157\x63\x61\x74\151\x6f\x6e\x3a\x20{$QCXRN}"); goto Fl6vw; W0Cq6: $Tbg4N = rand(0, $mbOKr - 1); goto IKAxK; IKAxK: $QCXRN = trim($EM0sT[$Tbg4N]); goto MypVi; gbPzB: $mbOKr = count($EM0sT); goto W0Cq6; Fl6vw: exit;
